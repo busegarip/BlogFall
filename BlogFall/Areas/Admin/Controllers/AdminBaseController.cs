f86d46e7954c683ec6d4263ad31e14eaa6344007
busegarip <buse_garip@hotmail.com>
2019-11-05 21:30:23 +0300

    YÖNETİM PANELİ DÜZENLEME

---
+++ BlogFall/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,23 @@
+﻿using BlogFall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogFall.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]//güvenlik için rolünde admin olanlar sadece bu controllıra girebilir veya miras alanlar ve kimliği doğrulanmış
+    public abstract class AdminBaseController : Controller
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                db.Dispose();
+            }
+        }
+    }
+}
